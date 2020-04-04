de762aba53fd8a5722b9a36de96a1483e647db62
Juan Carlos Gallegos <jgl400736@udelasalle.edu.mx>
2020-04-04 12:11:19 -0700

    Modificaciones al archivo ContentView

--- NetworkingDemoApp/ContentView.swift
+++ NetworkingDemoApp/ContentView.swift
@@ -8,10 +8,70 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(results, id: \.trackId){ item in
+                    VStack(alignment: .leading){
+                        Text(item.trackName)
+                            .font(.headline)
+                        Text(item.collectionName)
+                    }
+                }
+            }.onAppear(perform: loadData)
+            .navigationBarTitle("Songs List")
+        }
     }
+    
+    
+    func loadData(){
+        //Step 1
+        guard let url = URL (string:"https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print ("Invalid URL")
+            return
+        }
+        //Step 2
+        let request = URLRequest(url: url)
+        
+        //Step 3
+        URLSession.shared.dataTask(with: request){ data, response, error in
+            //Step 4
+            if let data2 = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data2) {
+                    
+                    DispatchQueue.main.async {
+                        //main thread
+                        self.results = decodedResponse.results
+                    }
+                    
+                    return
+                }
+                
+            }
+            
+            //Error
+            print ("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            
+        }.resume()
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
